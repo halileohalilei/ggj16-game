9ae1b1bc01839d5fbdfca87a783ab269dd2b3cdb
Halil İbrahim Kayım <kayimhalil@hotmail.com>
2016-01-30 03:41:50 +0200

    random obstacle and collectible generation

--- ggj16-game/Assets/Scripts/PlaneManager.cs
+++ ggj16-game/Assets/Scripts/PlaneManager.cs
@@ -1,16 +1,28 @@
 ﻿using UnityEngine;
+using System;
+using Random = System.Random;
 
 namespace Assets.Scripts
 {
     class PlaneManager : MonoBehaviour
     {
-        private const float SouthEnd = -50f;
-        private const float NorthEnd = 50f;
-        private const float EastEnd = 50f;
-        private const float WestEnd = -50f;
+        private const int SouthEnd = -50;
+        private const int NorthEnd = 50;
+        private const int EastEnd = 50;
+        private const int WestEnd = -50;
 
-        public GameObject ObstaclePrefab;
-        public GameObject CollectiblePrefab;
+        public GameObject ObstaclePrefab = null;
+        public GameObject CollectiblePrefab = null;
+
+        [SerializeField]
+        private int ObstacleCount = 0;
+        [SerializeField]
+        private int CollectibleCount = 0;
+
+        void Start()
+        {
+            GeneratePlane();
+        }
 
         public void GeneratePlane()
         {
@@ -22,12 +34,36 @@ namespace Assets.Scripts
 
         private void GenerateObstacles(GameLevel currentLevel)
         {
-            //TODO generate obstacles
+            var obstacleContainer = GameObject.Find("Obstacles").transform;
+            var xPositions = Util.GenerateRandomArray(WestEnd, EastEnd);
+            var zPositions = Util.GenerateRandomArray(SouthEnd, NorthEnd);
+            for (var i = 0; i < ObstacleCount; i++)
+            {
+                var randomPosition = new Vector3(xPositions[i], ObstaclePrefab.transform.position.y, zPositions[i]);
+                var currentObstacle = (GameObject) Instantiate(ObstaclePrefab, randomPosition, Quaternion.identity);
+
+                if (currentObstacle != null)
+                {
+                    currentObstacle.transform.parent = obstacleContainer;
+                }
+            }
         }
 
         private void GenerateCollectibles(GameLevel currentLevel)
         {
-            //TODO generate collectibles (lol)
+            var collectibleContainer = GameObject.Find("Collectibles").transform;
+            var xPositions = Util.GenerateRandomArray(WestEnd, EastEnd);
+            var zPositions = Util.GenerateRandomArray(SouthEnd, NorthEnd);
+            for (var i = 0; i < CollectibleCount; i++)
+            {
+                var randomPosition = new Vector3(xPositions[i], CollectiblePrefab.transform.position.y, zPositions[i]);
+                var currentCollectible = (GameObject)Instantiate(CollectiblePrefab, randomPosition, Quaternion.identity);
+
+                if (currentCollectible != null)
+                {
+                    currentCollectible.transform.parent = collectibleContainer;
+                }
+            }
         }
     }
 }
---
+++ ggj16-game/Assets/Scripts/Util.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class Util
+    {
+        private static Random random = new Random();
+
+        public static int[] GenerateRandomArray(int min, int max)
+        {
+            int arrayLength = max - min + 1;
+            var arr = new int[arrayLength];
+
+            for (var i = 0; i < arrayLength; i++)
+            {
+                arr[i] = i + min;
+            }
+
+            for (var i = 0; i < arrayLength; i++)
+            {
+                var randomIndex = random.Next(i, arrayLength - 1);
+                var temp = arr[i];
+                arr[i] = arr[randomIndex];
+                arr[randomIndex] = temp;
+            }
+
+            return arr;
+        }
+    }
+}
