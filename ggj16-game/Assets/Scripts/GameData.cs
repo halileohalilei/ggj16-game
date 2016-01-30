71039d29d67842aeacecdb7ad31026f7f4737eaa
Halil İbrahim Kayım <kayimhalil@hotmail.com>
2016-01-30 02:01:44 +0200

    added game data class

---
+++ ggj16-game/Assets/Scripts/GameData.cs
@@ -0,0 +1,32 @@
+﻿namespace Assets.Scripts
+{
+    class GameData
+    {
+        private static GameData _instance;
+
+        private GameLevel _currentLevel;
+        private int _pointsCollectedThisLevel;
+
+        public enum GameLevel
+        {
+            Jazz, Metal, Classic, Electronic
+        }
+
+        private GameData()
+        {
+            _pointsCollectedThisLevel = 0;
+        }
+
+        public static GameData GetInstance()
+        {
+            if (_instance == null)
+                _instance = new GameData();
+            return _instance;
+        }
+
+        public GameLevel GetCurrentLevel()
+        {
+            return _currentLevel;
+        }
+    }
+}
