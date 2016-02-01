634539e36600e6b5ff5ddfee30e9a79a4129a953
Halil İbrahim Kayım <kayimhalil@hotmail.com>
2016-02-01 16:58:15 +0200

    intro screen audio handling

--- ggj16-game/Assets/Scripts/ButtonHandler.cs
+++ ggj16-game/Assets/Scripts/ButtonHandler.cs
@@ -7,14 +7,18 @@ namespace Assets.Scripts
     public class ButtonHandler : MonoBehaviour
     {
         [SerializeField] private Image _fader;
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AudioClip _clickSound;
 
-        private Color _fadeColor = new Color(169f / 255f, 169f / 255f, 169f / 255f, 1f);
+        private readonly Color _fadeColor = new Color(169f / 255f, 169f / 255f, 169f / 255f, 1f);
+        private const float FadeSpeed = 5f;
 
         void Update()
         {
             if (_fader.IsActive())
             {
-                _fader.color = Color.Lerp(_fader.color, _fadeColor, Time.deltaTime * 5f);
+                _fader.color = Color.Lerp(_fader.color, _fadeColor, Time.deltaTime*FadeSpeed);
+                if (!_audioSource.clip) _audioSource.volume = Mathf.Lerp(_audioSource.volume, 0f, Time.deltaTime*FadeSpeed);
 
                 if (_fader.color.a > 0.99f)
                 {
@@ -28,6 +32,11 @@ namespace Assets.Scripts
             Debug.Log("Starting...");
             transform.GetComponent<Button>().interactable = false;
             _fader.gameObject.SetActive(true);
+        }
+
+        public void PlayClickSound()
+        {
+            _audioSource.PlayOneShot(_clickSound);
         }
     }
 }
