b4a628e9509984fa15949c70755cdbcf1a7635a8
Halil İbrahim Kayım <kayimhalil@hotmail.com>
2016-02-01 16:28:08 +0200

    intro screen completed

--- ggj16-game/Assets/Scripts/ButtonHandler.cs
+++ ggj16-game/Assets/Scripts/ButtonHandler.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class ButtonHandler : MonoBehaviour {
+namespace Assets.Scripts
+{
+    public class ButtonHandler : MonoBehaviour
+    {
+        [SerializeField] private Image _fader;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        private Color _fadeColor = new Color(169f / 255f, 169f / 255f, 169f / 255f, 1f);
+
+        void Update()
+        {
+            if (_fader.IsActive())
+            {
+                _fader.color = Color.Lerp(_fader.color, _fadeColor, Time.deltaTime * 5f);
+
+                if (_fader.color.a > 0.99f)
+                {
+                    SceneManager.LoadScene("game_scene");
+                }
+            }
+        }
+
+        public void StartGame()
+        {
+            Debug.Log("Starting...");
+            transform.GetComponent<Button>().interactable = false;
+            _fader.gameObject.SetActive(true);
+        }
+    }
 }
